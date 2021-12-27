dd5dc1319408013a5107d3e73f40632032571b05
Sofia Moniz <sofiamoniz@ua.pt>
2021-12-27 16:44:20 +0000

    pagination

--- src/pages/Drivers.js
+++ src/pages/Drivers.js
@@ -2,14 +2,17 @@ import React, {useEffect} from 'react';
 
 function Drivers(){
 
-    
     const [driversList, setDriversList] = React.useState([]);
+    const [pageSize, setPageSize] = React.useState(20);
+    const [page, setPage] = React.useState(1);
+
 
     const fetchState = async () => {
-        const fetchItem = await fetch('http://192.168.160.58/Formula1/api/Drivers');
+        const fetchItem = await fetch('http://192.168.160.58/Formula1/api/Drivers?page=' + page + '&pagesize=' + pageSize);
         const item = await fetchItem.json();
         setDriversList(item.List);
     };
+
     
     const retrieveDrivers = () => {
         return (
@@ -23,7 +26,7 @@ function Drivers(){
                     </thead>
                     <tbody>
                         {driversList.map(driver =>
-                            <tr>
+                            <tr key={driver.id}>
                                 <td>{driver.Name}</td>
                                 <td>{driver.Nationality}</td>
                             </tr>
@@ -33,16 +36,34 @@ function Drivers(){
             </div>
         )
     }
-    
 
+    const changePageForward = () => {
+        setPage(page+1);
+        fetchState();
+    }
+
+    const changePageBackward = () => {
+        if(page > 1){
+            setPage(page-1);
+            fetchState();
+        }
+    }
+    
     useEffect(() => {
         fetchState();
+        
     },[])
 
     return(
         <div>
             <h1>Get to know the drivers</h1>
             {retrieveDrivers()}
+            <nav aria-label="Page navigation example">
+                <ul className="pagination">
+                    <li className="page-item"><a className="page-link" onClick={changePageBackward}>Previous</a></li>
+                    <li className="page-item"><a className="page-link" onClick={changePageForward}>Next</a></li>
+                </ul>
+            </nav>
         </div>
         
     )
